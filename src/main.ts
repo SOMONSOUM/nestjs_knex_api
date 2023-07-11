18e4fea99472617c4b8c1dc409794b686b27e4ec
SOM ON SOUM <somon.soum9@gmail.com>
2023-07-11 16:50:56 +0700

    fix csurf

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import { ValidationPipe } from '@nestjs/common';
-import csurf from 'csurf';
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(csurf());
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
